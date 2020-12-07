68609a548bd8145377e8c09bc06e0ae816e4efcc
Andrew Craig <andrewsc@gmail.com>
2020-12-07 08:54:03 +0000

    Day 7

--- src/main/kotlin/dev/acraig/adventofcode/y2020/Day7.kt
+++ src/main/kotlin/dev/acraig/adventofcode/y2020/Day7.kt
@@ -4,28 +4,29 @@ import java.nio.file.Files
 import java.nio.file.Path
 
 data class BagDefinition(val name: String, val bagTypes: Map<String, Int>) {
-    fun canContainShinyGold(allDefinitions:Map<String, BagDefinition>):Boolean {
-        return bagTypes.containsKey("shiny gold") || bagTypes.keys.any { allDefinitions[it]!!.canContainShinyGold(allDefinitions) }
-    }
-    fun countNestedBags(allDefinitions: Map<String, BagDefinition>): Int {
-        return if (bagTypes.isEmpty()) {
-            0
-        } else {
-            bagTypes.map { (key, value) ->
-                value + (allDefinitions[key]!!.countNestedBags(allDefinitions) * value)
-            }.sum()
+    fun canContainShinyGold(allDefinitions: Map<String, BagDefinition>): Boolean {
+        return bagTypes.containsKey("shiny gold") || bagTypes.keys.any {
+            allDefinitions[it]!!.canContainShinyGold(
+                allDefinitions
+            )
         }
+    }
+
+    fun countNestedBags(allDefinitions: Map<String, BagDefinition>): Int {
+        return bagTypes.map { (key, value) ->
+            value + (allDefinitions[key]!!.countNestedBags(allDefinitions) * value)
+        }.sum()
     }
 }
 
-fun day7Part1(path:Path):Int {
+fun day7Part1(path: Path): Int {
     val bagDefinitions = readFile(path)
-    return bagDefinitions.values.map() {
-        bag -> bag.canContainShinyGold(bagDefinitions)
+    return bagDefinitions.values.map() { bag ->
+        bag.canContainShinyGold(bagDefinitions)
     }.count { it }
 }
 
-fun day7Part2(path:Path):Int {
+fun day7Part2(path: Path): Int {
     val bagDefinitions = readFile(path)
     return bagDefinitions["shiny gold"]!!.countNestedBags(bagDefinitions)
 }
